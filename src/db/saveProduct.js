20d66c4ebb503c07edc19968763e7bfcb514d6ac
Andrés Pinto <andres.pinto.n@gmail.com>
2024-05-14 17:56:53 -0500

    product

--- src/db/saveProduct.js
+++ src/db/saveProduct.js
@@ -5,20 +5,20 @@ dotenv.config();
 
 
 // Function to save the response to the database
-export const saveProduct = async ({ name, description, meta_title, image1, link, short_description, meta_description, price }) => {
+export const saveProduct = async ({ name, description, meta_title, image1, link, short_description, meta_description, price, old_price }) => {
  try {
 
   let data
 
   data = {
-   name: name[0],
-   description: description[0],
-   meta_title: meta_title[0],
-   meta_description: meta_description[0],
-   image1: image1[0],
-   link: link[0],
-   short_description: short_description[0],
-   price: price[0]
+   name: name[0] || "",
+   description: description[0] || "",
+   meta_title: meta_title[0] || "",
+   meta_description: meta_description[0] || "",
+   image1: image1[0] || "",
+   link: link[0] || "",
+   short_description: short_description[0] || "",
+   price: price[0] || old_price
 
   }
   // Verificar si el enlace del producto ya existe en la base de datos
