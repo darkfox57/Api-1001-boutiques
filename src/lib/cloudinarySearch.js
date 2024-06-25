699ac77466fbbe657d26de3a5194d315143d40ab
Andrés Pinto <andres.pinto.n@gmail.com>
2024-06-25 15:39:37 -0500

    imagenes

--- src/lib/cloudinarySearch.js
+++ src/lib/cloudinarySearch.js
@@ -3,37 +3,41 @@ import cloudinary from "./cloudinaryUpload.js";
 export default async function imgSearch(brand, collection, type) {
  try {
   const searchImage = async (exp) => {
-   const res = await cloudinary.search.expression(exp).execute();
+   const res = await cloudinary.search.expression(exp).fields('secure_url').fields('tags').execute();
    if (res.resources.length > 0) {
     let filteredResources = res.resources;
-    if (brand) {
-     filteredResources = res.resources.filter((item) => item.asset_folder === brand);
+    if (collection && brand) {
+     filteredResources = res.resources.filter((item) =>
+      item.asset_folder === brand && item.tags.includes(collection)
+     );
     }
     const randomIndex = Math.floor(Math.random() * filteredResources.length);
     return filteredResources[randomIndex].secure_url;
+    // return filteredResources
    }
    return null;
   };
 
-  let exp = `resource_type:image`;
+  let exp = `resource_type=image`;
 
-  if (collection) exp += ` AND (tags=${collection})`;
+  if (collection) exp += ` AND (tags=${collection}*)`;
 
-  if (brand) exp += ` AND (tags=${brand})`;
+  if (brand && !collection) exp += ` AND folder=${brand}`;
 
-  if (!brand && !collection && !type) exp = `folder:Z8SHOPPING AND resource_type:image`
+  if (!brand && !collection && !type) exp = `folder=Z8SHOPPING AND resource_type:image`
 
   let img = await searchImage(exp);
+
   if (!img && brand) {
-   exp = `resource_type:image AND tags=${brand}`;
+   exp = `resource_type:image AND tags=${brand}*`;
    img = await searchImage(exp);
   }
   if (!img && type) {
-   exp = `resource_type:image AND tags=${type}`;
+   exp = `resource_type:image AND tags=${type}*`;
    img = await searchImage(exp);
   }
   if (!img) {
-   exp = `folder:Z8SHOPPING AND resource_type:image`;
+   exp = `folder=Z8SHOPPING AND resource_type:image`;
    img = await searchImage(exp);
   }
   return img;
