6d8686208d0b630c32f8b6198fd8c89e16d7f86d
Andrés Pinto <andres.pinto.n@gmail.com>
2024-07-23 22:45:21 -0500

    gemini y open ia comment

--- src/controllers/content-generators/gemini.js
+++ src/controllers/content-generators/gemini.js
@@ -1,45 +1,45 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
-import dotenv from 'dotenv';
-dotenv.config();
+// import { GoogleGenerativeAI } from "@google/generative-ai";
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-const api_key = process.env.GOOGLE_API_KEY;
-const genAI = new GoogleGenerativeAI(api_key);
-const generationConfig = { temperature: 0.9, topP: 1, topK: 50, maxOutputTokens: 4096 };
+// const api_key = process.env.GOOGLE_API_KEY;
+// const genAI = new GoogleGenerativeAI(api_key);
+// const generationConfig = { temperature: 0.9, topP: 1, topK: 50, maxOutputTokens: 4096 };
 
-const model = genAI.getGenerativeModel({ model: "gemini-pro", generationConfig });
+// const model = genAI.getGenerativeModel({ model: "gemini-pro", generationConfig });
 
-export async function geminiGenerator({ content }) {
-  try {
-    const prompt = `
-    You're a seasoned copywriter tasked with revamping an existing blog post. Your mission? To craft a masterpiece that not only engages readers but also dominates search engine results.
+// export async function geminiGenerator({ content }) {
+//   try {
+//     const prompt = `
+//     You're a seasoned copywriter tasked with revamping an existing blog post. Your mission? To craft a masterpiece that not only engages readers but also dominates search engine results.
 
-    Here's what you need to do:
+//     Here's what you need to do:
 
-    Content Audit: Start by thoroughly analyzing the current article. What are its strengths and weaknesses? Are there any knowledge gaps or outdated information?
-    Structure & Flow: Revamp the structure for optimal readability. Consider catchy headlines, subheadings, bullet points, and numbered lists to break up text and guide readers.
-    Tone & Voice: Infuse the content with a captivating tone that aligns with the target audience. Is it informative and professional? Playful and conversational? Tailor it to resonate with your readers.
-    SEO Optimization: Integrate relevant keywords naturally throughout the text. Utilize meta descriptions and header tags strategically to boost search engine ranking.
-    Content Expansion (Optional): Don't be afraid to add fresh content! This could include new data, case studies, expert quotes, or even multimedia elements like infographics or videos to enhance user experience.
-    Deliverables:
+//     Content Audit: Start by thoroughly analyzing the current article. What are its strengths and weaknesses? Are there any knowledge gaps or outdated information?
+//     Structure & Flow: Revamp the structure for optimal readability. Consider catchy headlines, subheadings, bullet points, and numbered lists to break up text and guide readers.
+//     Tone & Voice: Infuse the content with a captivating tone that aligns with the target audience. Is it informative and professional? Playful and conversational? Tailor it to resonate with your readers.
+//     SEO Optimization: Integrate relevant keywords naturally throughout the text. Utilize meta descriptions and header tags strategically to boost search engine ranking.
+//     Content Expansion (Optional): Don't be afraid to add fresh content! This could include new data, case studies, expert quotes, or even multimedia elements like infographics or videos to enhance user experience.
+//     Deliverables:
 
-    Polished and SEO-optimized blog post (minimum 2000 words) and return it in html format, you dont need to generate de html, head and body tag just the content tags (this is very important).
-    Bonus Points:
+//     Polished and SEO-optimized blog post (minimum 2000 words) and return it in html format, you dont need to generate de html, head and body tag just the content tags (this is very important).
+//     Bonus Points:
 
-    By following these guidelines, you'll transform the blog post into a powerful tool that attracts, informs, and converts readers. Good luck!
+//     By following these guidelines, you'll transform the blog post into a powerful tool that attracts, informs, and converts readers. Good luck!
 
-    this is the text you need to review:
-    ${content}
-    `;
+//     this is the text you need to review:
+//     ${content}
+//     `;
 
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
+//     const result = await model.generateContent(prompt);
+//     const response = await result.response;
+//     const text = response.text();
 
-    const article = text.toString();
+//     const article = text.toString();
 
-    return article
+//     return article
 
-  } catch (err) {
-    console.log(err.message);
-  }
-}
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// }
--- src/controllers/content-generators/openai.js
+++ src/controllers/content-generators/openai.js
@@ -1,111 +1,111 @@
 
-import dotenv from 'dotenv';
-import OpenAI from 'openai';
-import { saveBlog } from "../../db/saveBlog.js";
+// import dotenv from 'dotenv';
+// import OpenAI from 'openai';
+// import { saveBlog } from "../../db/saveBlog.js";
 
 
 
-dotenv.config();
+// dotenv.config();
 
-const openAIInstance = new OpenAI({ apiKey: process.env.OPEN_IA_API });
+// const openAIInstance = new OpenAI({ apiKey: process.env.OPEN_IA_API });
 
-export async function openaiGenerator(req, res) {
- try {
-  const seo = await openAIInstance.chat.completions.create({
-   messages: [
-    {
-     role: "system",
-     content: `you are a helpful assistant designet to output JSON.`,
-    },
-    {
-     role: "user", content: `{
-      title: "Title of the article",
-      description: "Description of the item (maximum 160 characters)",
-      slug: "article-slug",
-      category: "article category"
-      tags: ["tag1", "tag2", "tag3"],
-      content: "     
+// export async function openaiGenerator(req, res) {
+//  try {
+//   const seo = await openAIInstance.chat.completions.create({
+//    messages: [
+//     {
+//      role: "system",
+//      content: `you are a helpful assistant designet to output JSON.`,
+//     },
+//     {
+//      role: "user", content: `{
+//       title: "Title of the article",
+//       description: "Description of the item (maximum 160 characters)",
+//       slug: "article-slug",
+//       category: "article category"
+//       tags: ["tag1", "tag2", "tag3"],
+//       content: "     
          
-     Write the introduction of the article here.
+//      Write the introduction of the article here.
      
-     <h2> Subtitle 1 </h2>
+//      <h2> Subtitle 1 </h2>
      
-     Write the content of subtitle 1 here.
+//      Write the content of subtitle 1 here.
      
-     <h3> Subtitle 1.1</h3>
+//      <h3> Subtitle 1.1</h3>
      
-     Write the content of subtitle 1.1 here.
+//      Write the content of subtitle 1.1 here.
 
-     <div id='ads'>do not write anything here</div>
+//      <div id='ads'>do not write anything here</div>
      
-     <h4> Subtitle 2 </h4>
+//      <h4> Subtitle 2 </h4>
      
-     Write the content of subtitle 2 here.
+//      Write the content of subtitle 2 here.
      
-     <h4> Subtitle 2.1 </h4>
+//      <h4> Subtitle 2.1 </h4>
      
-     Write the content of subtitle 2.1 here.
+//      Write the content of subtitle 2.1 here.
      
-     <h4> Conclusion </h4>
+//      <h4> Conclusion </h4>
      
-     Write the conclusion of the article here.
+//      Write the conclusion of the article here.
      
-     *Note:* You can add more subheadings and sections as needed.
-     "
-     }
+//      *Note:* You can add more subheadings and sections as needed.
+//      "
+//      }
      
-     Instructions:
-     Choose one of the following themes: spécifités des soutiens gorge de la marque de lingerie empreinte
-     Fill in the "title", "description", "slug", "category" and "tags", make sure about filling all the fields of the JSON do not forget de description.
-     Write the content of the article with at least 2000 words in html format in the "content" field.
-     Use relevant keywords in the title, description, tags, and article content.
-     Make sure the title is catchy and the description is informative.
-     Use a clear and organized structure in the content of the article. freely include lists, dates and everything you need.
-     Include images in the middle of the content to improve the user experience.
-     Finally it returns only the JSON all the content must be in french`
-    },
-   ],
-   // prompt: `transform this to json: ${response}`,
-   model: "gpt-4-turbo",
-   response_format: { type: "json_object" },
-  });
+//      Instructions:
+//      Choose one of the following themes: spécifités des soutiens gorge de la marque de lingerie empreinte
+//      Fill in the "title", "description", "slug", "category" and "tags", make sure about filling all the fields of the JSON do not forget de description.
+//      Write the content of the article with at least 2000 words in html format in the "content" field.
+//      Use relevant keywords in the title, description, tags, and article content.
+//      Make sure the title is catchy and the description is informative.
+//      Use a clear and organized structure in the content of the article. freely include lists, dates and everything you need.
+//      Include images in the middle of the content to improve the user experience.
+//      Finally it returns only the JSON all the content must be in french`
+//     },
+//    ],
+//    // prompt: `transform this to json: ${response}`,
+//    model: "gpt-4-turbo",
+//    response_format: { type: "json_object" },
+//   });
 
 
 
-  const text = JSON.parse(seo.choices[0].message.content)
+//   const text = JSON.parse(seo.choices[0].message.content)
 
-  // const article = await geminiGenerator(text)
+//   // const article = await geminiGenerator(text)
 
-  // res.status(201).json(article);
+//   // res.status(201).json(article);
 
-  const post = {
-   title: text.title,
-   description: text.description,
-   tags: text.tags,
-   category: [text.category],
-   slug: text.slug,
-   content: text.content,
-   published: true,
-   img: 'https://images.unsplash.com/photo-1552581234-26160f608093?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
-  };
+//   const post = {
+//    title: text.title,
+//    description: text.description,
+//    tags: text.tags,
+//    category: [text.category],
+//    slug: text.slug,
+//    content: text.content,
+//    published: true,
+//    img: 'https://images.unsplash.com/photo-1552581234-26160f608093?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80'
+//   };
 
 
 
-  // Verificar si alguno de los elementos del objeto está vacío
-  const emptyFields = Object.entries(post)
-   .filter(([key, value]) => value === undefined || value === null || value === '')
-   .map(([key, value]) => key);
+//   // Verificar si alguno de los elementos del objeto está vacío
+//   const emptyFields = Object.entries(post)
+//    .filter(([key, value]) => value === undefined || value === null || value === '')
+//    .map(([key, value]) => key);
 
-  if (emptyFields.length > 0) {
-   const errorMessage = `Los siguientes campos están incompletos: ${emptyFields.join(', ')}`;
-   throw new Error(errorMessage);
-  } else {
-   await saveBlog(post);
-   res.status(201).json(post);
-  }
+//   if (emptyFields.length > 0) {
+//    const errorMessage = `Los siguientes campos están incompletos: ${emptyFields.join(', ')}`;
+//    throw new Error(errorMessage);
+//   } else {
+//    await saveBlog(post);
+//    res.status(201).json(post);
+//   }
 
- } catch (e) {
-  res.status(500).send(e.message)
- }
+//  } catch (e) {
+//   res.status(500).send(e.message)
+//  }
 
-} 
+// } 
