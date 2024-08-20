dcb5cec9901687668698ba1be76bd392d069b9ea
Andrés Pinto <andres.pinto.n@gmail.com>
2024-08-20 08:06:57 -0500

    product by tag

--- src/controllers/product/getProductByTag.js
+++ src/controllers/product/getProductByTag.js
@@ -5,50 +5,46 @@ import models from '../../db.js';
 dotenv.config();
 
 const getProductByTag = async (req, res) => {
- const { brand, collection } = req.query;
+ const { brand, collection, type, type_form } = req.query;
 
  try {
-  let data = []
+  let whereConditions = {};
 
-  const exactMatch = await models.Product.findAll({
-   where: {
-    [Op.and]: [
-     Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('collection')), {
-      [Op.like]: `%${collection.toLowerCase()}%`
-     }),
-     Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('brand')), {
-      [Op.like]: `%${brand.toLowerCase()}%`
-     })
-    ]
-   },
-   order: Sequelize.literal('rand()'),
-   limit: 8
-  });
+  // 1. Si hay marca, buscar por marca
+  if (brand) {
+   whereConditions.brand = brand;
 
-  // Si se encuentra una coincidencia exacta, se agrega a los resultados
-  if (exactMatch.length > 0) {
-   data = exactMatch;
-  } else {
-   // Si no se encuentra una coincidencia exacta, busca productos que contengan todas las etiquetas en el nombre
-   data = await models.Product.findAll({
-    where: {
-     brand: brand,
-     // name: {
-     //  [Op.like]: collection
-     // }
-    },
-    order: Sequelize.literal('rand()'),
-    limit: 8
-   });
+   // 2. Si hay colección, buscar por marca y colección
+   if (collection) {
+    whereConditions.collection = collection;
+   }
+
+   // 3. Si hay tipo, buscar por marca, colección y tipo
+   if (type) {
+    whereConditions.type = { [Op.like]: `%${type}%` };
+   }
+  } else if (type) {
+   // 4. Si no hay marca pero hay tipo, buscar por tipo
+   whereConditions.type = { [Op.like]: `%${type}%` };
+
+   // 5. Si hay type_form, buscar por tipo y type_form
+   if (type_form) {
+    whereConditions.type_form = { [Op.like]: `%${type_form}%` };
+   }
   }
+
+  // Realizar la consulta a la base de datos con las condiciones construidas
+  const data = await models.Product.findAll({
+   where: whereConditions,
+   order: Sequelize.literal('RAND()'),  // Seleccionar productos aleatoriamente
+   limit: 8  // Limitar el número de resultados a 8
+  });
 
   res.status(200).json(data);
  } catch (error) {
   console.error("Error al consultar los productos:", error);
   res.status(500).json({ error: "Error al consultar los productos" });
  }
-}
+};
 
-
-
-export default getProductByTag
+export default getProductByTag;
