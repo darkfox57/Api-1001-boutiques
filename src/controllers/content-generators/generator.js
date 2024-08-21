9e0a765667b409ac2d57ecdf8e2ba8cc8ecfb4f2
Andrés Pinto <andres.pinto.n@gmail.com>
2024-08-21 02:51:05 -0500

    prompt

--- src/controllers/content-generators/generator.js
+++ src/controllers/content-generators/generator.js
@@ -12,7 +12,7 @@ export async function generator(req, res) {
 
   const prompt = `You are a talented and versatile content writer tasked with generating a comprehensive blog article by the topic: ${keywords}. Your article should include the following elements:
 
-    Title: Generate a catchy and SEO-friendly title for the blog article. avoid the use of the words confort and elegance.
+    Title: Generate a catchy and SEO-friendly title for the blog article. it's important to avoid using of the words confort and elegance.
     Slug: Create a URL-friendly slug based on the title.
     Description: Write a compelling meta description that accurately summarizes the article's content and entices readers to engage with it.
     Tags: Provide a list of relevant tags or keywords related to the article's subject matter. If there is a brand name or collection, place it first in the tags, followed by the rest of the tags.
